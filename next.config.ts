bd4bd8fc62c33026f2185a90ec1b853e97ccd22d
zoxknez <zoxknez@hotmail.com>
2025-10-15 23:02:34 +0200

    security: Add basic HTTP security headers

--- next.config.ts
+++ next.config.ts
@@ -27,6 +27,31 @@ const nextConfig: NextConfig = {
     }
     return [];
   },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'X-DNS-Prefetch-Control',
+            value: 'on'
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'SAMEORIGIN'
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff'
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'strict-origin-when-cross-origin'
+          }
+        ]
+      }
+    ];
+  },
 };
 
 export default nextConfig;
